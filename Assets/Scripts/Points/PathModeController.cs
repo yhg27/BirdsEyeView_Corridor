5dc8f201a4e74cbb0507381fef75cb081ff859f8
niklassindemann-ctrl <niklas.sindemann@gmail.com>
2025-12-06 16:33:53 +0000

    Fixed B Button

--- Assets/Scripts/Points/PathModeController.cs
+++ Assets/Scripts/Points/PathModeController.cs
@@ -28,8 +28,6 @@ namespace Points
 		private InputDevice _rightHand;
 		private InputDevice _leftHand;
 		private bool _rightGripPrev;
-		private bool _bButtonPrev;
-		private bool _aButtonPrev;
 		private bool _triggerPrev;
 		
 		// When set, indicates we want to continue the route starting from this point
@@ -143,15 +141,9 @@ namespace Points
 				HandleTriggerInput();
 			}
 
-			// Handle B button for undo
-			bool bButton = ReadButton(_rightHand, CommonUsages.secondaryButton);
-			if (EdgePressed(bButton, ref _bButtonPrev))
-			{
-				_pathManager.UndoLastPoint();
-				ProvideHapticFeedback(_hapticAmplitude * 0.5f, _hapticDuration);
-			}
-
-			// A button is used for depth control in normal mode - not used in path mode
+			// B button is handled by RayDepthController for depth adjustment (move ghost closer)
+			// A button is also handled by RayDepthController for depth adjustment (move ghost further)
+			// Removed undo functionality - B button should only adjust ghost depth in path mode
 
 			// DISABLED: Grip for finishing route (now used for path mode toggle)
 			// bool gripButton = ReadButton(_rightHand, CommonUsages.gripButton);
@@ -1186,7 +1178,7 @@ namespace Points
 				return "Click another point to continue the route";
 			}
 
-			return "Click points to extend route, B to undo";
+			return "Click points to extend route";
 		}
 	}
 }
